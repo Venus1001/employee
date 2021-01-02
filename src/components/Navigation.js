bd3c6282300236045ba059c5fbe2e9f57cb8c212
venus <autumn.qiu@icloud.come>
2021-01-02 11:16:59 -0500

    commit updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ function Navigation({ onSearch, searchTerm, handleSortByName, handleSortByDept }
         <div className="md:flex justify-between my-6">
             <div className="md:mb-0 mb-4">
                 <button onClick={handleSortByName} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 mr-4 hover:bg-yellow-500 hover:text-white">Name</button>
-                <button onClick={handleSortByDept} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 hover:bg-yellow-500 hover:text-white">Dept</button>
+                <button onClick={handleSortByDept} className="bg-white focus:bg-yellow-500 focus:text-white focus:outline-none shadow font-bold text-gray-800 rounded px-8 py-1 hover:bg-yellow-500 hover:text-white">Department</button>
             </div>
             <form>
                 <input
@@ -13,7 +13,7 @@ function Navigation({ onSearch, searchTerm, handleSortByName, handleSortByDept }
                     onChange={onSearch} 
                     className="shadow pl-5 rounded w-full py-1"
                     type="text"
-                    placeholder="search employee"/>
+                    placeholder="search employees"/>
             </form>
         </div>
     )
